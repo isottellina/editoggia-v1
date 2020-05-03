4aa8082a92313abaae49674a4572064ff5c3f331
Louise <louise@zanier.org>
2020-05-03 06:43:49 +0200

    Added some javascript and the navbar

--- app/assets.py
+++ app/assets.py
@@ -3,7 +3,7 @@
 # Filename: assets.py
 # Author: Louise <louise>
 # Created: Sat May  2 05:38:44 2020 (+0200)
-# Last-Updated: Sun May  3 06:17:51 2020 (+0200)
+# Last-Updated: Sun May  3 06:30:18 2020 (+0200)
 #           By: Louise <louise>
 # 
 """
@@ -18,5 +18,11 @@ css = Bundle(
     depends=('**/*.scss')
 )
 
+js = Bundle(
+    'js/ui.js',
+    output='js/scripts.js'
+)
+
 assets = Environment()
 assets.register('css_all', css)
+assets.register('js_all', js)
