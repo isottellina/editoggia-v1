f2a53d763166f6f4b72411d0c85ebf278a7a0684
Louise <louise@zanier.org>
2020-05-04 03:43:39 +0200

    Add non-nullables

--- app/users/models.py
+++ app/users/models.py
@@ -3,7 +3,7 @@
 # Filename: models.py
 # Author: Louise <louise>
 # Created: Mon May  4 01:45:09 2020 (+0200)
-# Last-Updated: Mon May  4 03:30:24 2020 (+0200)
+# Last-Updated: Mon May  4 03:42:59 2020 (+0200)
 #           By: Louise <louise>
 # 
 from flask_login import UserMixin
@@ -13,34 +13,37 @@ from app.extensions import db
 class RolesUsers(db.Model):
     __tablename__ = 'roles_users'
     id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
-    role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+    user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'), nullable=False)
 
 class Role(db.Model):
     __tablename__ = 'role'
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    
+    permissions = db.relationship('Permission', secondary='permissions_roles',
+                                  backref=db.backref('roles', lazy='dynamic'))
 
 class PermissionsRoles(db.Model):
     __tablename__ = 'permissions_roles'
     id = db.Column(db.Integer(), primary_key=True)
-    role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-    perm_id = db.Column('perm_id', db.Integer(), db.ForeignKey('permission.id'))
+    role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'), nullable=False)
+    perm_id = db.Column('perm_id', db.Integer(), db.ForeignKey('permission.id'), nullable=False)
 
 class Permission(db.Model):
     __tablename__ = 'permission'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    description = db.Column(db.String(255))
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     
-    email = db.Column(db.String(255), unique=True)
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     
     last_login_at = db.Column(db.DateTime())
     current_login_at = db.Column(db.DateTime())
