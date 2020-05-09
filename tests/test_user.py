e0dd04ff37a8d9d9faf6e774344750e35f3bef4d
Louise <louise@zanier.org>
2020-05-09 20:09:43 +0200

    Added tests for register view

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,7 +3,7 @@
 # Filename: test_user.py
 # Author: Louise <louise>
 # Created: Fri May  8 20:30:10 2020 (+0200)
-# Last-Updated: Sat May  9 19:57:37 2020 (+0200)
+# Last-Updated: Sat May  9 20:09:31 2020 (+0200)
 #           By: Louise <louise>
 #
 """
@@ -58,6 +58,24 @@ class TestUser(unittest.TestCase):
             User.query.filter(User.username == username).exists()
         ).scalar()
 
+    def test_signup_short_username(self):
+        """
+        Tests that a 1-character username is refused.
+        """
+        username = 'a'
+        email = fake.email()
+        password = fake.password()
+
+        rv = self.register(username, email, password)
+
+        assert rv._status_code == 200
+        assert b"Username must be between 2 and 50 characters." in rv.data
+
+        # Check the user does not exist in the database
+        assert not db.session.query(
+            User.query.filter(User.username == username).exists()
+        ).scalar()
+        
     def test_signup_bad_email(self):
         """
         Tests that a bad email is refused.
@@ -70,6 +88,70 @@ class TestUser(unittest.TestCase):
 
         assert rv._status_code == 200
         assert b"Email must be valid." in rv.data
+
+        # Check the user does not exist in the database
+        assert not db.session.query(
+            User.query.filter(User.username == username).exists()
+        ).scalar()
+
+    def test_signup_empty_email(self):
+        """
+        Tests that an empty email is refused.
+        """
+        username = fake.user_name()
+        email = ""
+        password = fake.password()
+
+        rv = self.register(username, email, password)
+
+        assert rv._status_code == 200
+        assert b"Email must be filled." in rv.data
+
+        # Check the user does not exist in the database
+        assert not db.session.query(
+            User.query.filter(User.username == username).exists()
+        ).scalar()
+
+    def test_signup_empty_email(self):
+        """
+        Tests that an email longer than 128 is refused.
+        """
+        username = fake.user_name()
+        email = "{}@gmail.com".format('a' * 128)
+        password = fake.password()
+
+        rv = self.register(username, email, password)
+
+        assert rv._status_code == 200
+        assert b"Email must be less than 128 characters." in rv.data
+
+        # Check the user does not exist in the database
+        assert not db.session.query(
+            User.query.filter(User.username == username).exists()
+        ).scalar()
+
+    def test_signup_bad_confirm_password(self):
+        """
+        Tests that the confirm password field is checked. With this
+        test, we normally have tested all the validators used, and
+        thus, should be good to go.
+        """
+        username = fake.user_name()
+        email = fake.email()
+        password = fake.password()
+
+        # We have to do this request ourselves since
+        # the helper functions abstracts the confirm
+        # password field for us.
+        rv = self.app.post('/signup', data={
+            "username": username,
+            "email": email,
+            "password": password,
+            "confirm": password[:len(password) // 2]
+        }, follow_redirects=True)
+
+        assert rv._status_code == 200
+        assert b"Passwords must match." in rv.data
 
         # Check the user does not exist in the database
         assert not db.session.query(
