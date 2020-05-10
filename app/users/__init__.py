ac53a09feff25c617d6e309bc4ed3358809a582a
Louise <louise@zanier.org>
2020-05-10 22:55:09 +0200

    Began user blueprint

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -3,13 +3,14 @@
 # Filename: __init__.py
 # Author: Louise <louise>
 # Created: Mon May  4 01:42:51 2020 (+0200)
-# Last-Updated: Sat May  9 19:16:35 2020 (+0200)
+# Last-Updated: Sun May 10 22:48:55 2020 (+0200)
 #           By: Louise <louise>
 # 
 from flask import Blueprint
 
 users = Blueprint('users',
                   __name__,
+                  url_prefix='/user',
                   template_folder='templates',
                   static_folder='static')
 
