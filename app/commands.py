7fe54b41db2e93b7df42de5ce9a5ebdf4f84ff61
Louise <louise@zanier.org>
2020-05-15 22:24:21 +0200

    Faked fictions into the DB

--- app/commands.py
+++ app/commands.py
@@ -3,19 +3,21 @@
 # Filename: commands.py
 # Author: Louise <louise>
 # Created: Fri May  8 20:45:27 2020 (+0200)
-# Last-Updated: Fri May 15 15:05:29 2020 (+0200)
+# Last-Updated: Fri May 15 22:23:07 2020 (+0200)
 #           By: Louise <louise>
 # 
 import click
 import flask_migrate
+from sqlalchemy.sql.expression import func
 from flask_babel import gettext
 from faker import Faker
 
 from app.database import db
 from app.user.models import User, Role, Permission
+from app.story.models import FandomCategory, Fandom, Fiction, Chapter
 
 @click.option('--num_users', default=5, help='Number of users.')
-def populate_db(num_users):
+def populate_db_users(num_users):
     """
     Populates the database with fake data from faker
     """
@@ -50,7 +52,50 @@ def populate_db(num_users):
         db.session.add(user)
     db.session.commit()
 
+@click.option('--num_fictions', default=10, help='Number of fictions')
+def populate_db_fictions(num_fictions):
+    """
+    Populate the database with an appropriate number of fictions, written by
+    random users. It associates every fiction with the Original work fandom.
+    """
+    fake = Faker()
+
+    fandom = db.session.query(Fandom).filter(Fandom.name == "Original Work") \
+                                     .first()
+    assert fandom
+    
+    for _ in range(num_fictions):
+        author = db.session.query(User).order_by(func.random()).first()
+
+        fiction = Fiction(
+            name=fake.sentence(),
+            author=author,
+            fandom=[fandom]
+        )
+        db.session.add(fiction)
+
+        only_chapter = Chapter(
+            nb=0,
+            summary=" ".join(fake.sentences(nb=5)),
+            content=fake.text(max_nb_chars=3000),
+            fiction=fiction
+        )
+        db.session.add(only_chapter)
+
+    db.session.commit()
+        
+@click.option('--num_users', default=5, help='Number of users')
+@click.option('--num_fictions', default=10, help='Number of fictions')
+def populate_db(num_users, num_fictions):
+    populate_db_users(num_users)
+    populate_db_fictions(num_fictions)
+    
 def create_db():
+    """
+    Creates a DB and populates it with the bare minimum with
+    testing. This should not be used in production, a better
+    solution will be found.
+    """
     flask_migrate.upgrade()
 
     admin_role = db.session.query(Role).filter(Role.name=="Administrator") \
@@ -70,10 +115,24 @@ def create_db():
             permissions=[admin_perm]
         )
         db.session.add(admin_role)
+
+    other_category = db.session.query(FandomCategory) \
+                               .filter(FandomCategory.name == "Other") \
+                               .first()
+    if not other_category:
+        category = FandomCategory(
+            name="Other"
+        )
+        db.session.add(category)
+
+        fandom = Fandom(
+            name="Original Work",
+            category=category
+        )
+        db.session.add(fandom)
+
+    db.session.commit()
         
-        # Commit session
-        db.session.commit()
-    
 # Various helpers
 @click.argument('username')
 def set_admin(username):
@@ -92,6 +151,8 @@ def register_commands(app):
     """
     Register all custom commands for the Flask CLI.
     """
+    app.cli.command()(populate_db_users)
+    app.cli.command()(populate_db_fictions)
     app.cli.command()(populate_db)
     app.cli.command()(create_db)
     app.cli.command()(set_admin)
