246d6bfeadeb06d0b970ce5810adad89f9f0e4b2
Louise <louise@zanier.org>
2020-05-22 19:21:54 +0200

    Added a form to post a story

--- editoggia/story/views.py
+++ editoggia/story/views.py
@@ -3,16 +3,16 @@
 # Filename: views.py
 # Author: Louise <louise>
 # Created: Thu May 14 18:26:12 2020 (+0200)
-# Last-Updated: Fri May 22 19:09:34 2020 (+0200)
+# Last-Updated: Fri May 22 19:21:00 2020 (+0200)
 #           By: Louise <louise>
 #
 from flask import render_template, redirect, url_for, abort
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from editoggia.database import db
 from editoggia.story import story
 from editoggia.story.forms import StoryForm
-from editoggia.models.story import Story, Chapter
+from editoggia.models.story import Fandom, Story, Chapter
 
 @story.route('/')
 def index():
@@ -30,8 +30,34 @@ def post_story():
     Post a new story.
     """
     form = StoryForm()
+
+    # We have to populate the fandom field
+    fandoms = db.session.query(Fandom).all()
+    form.fandom.choices = [
+        (fandom.name, fandom.name) for fandom in fandoms
+    ]
+    
     if form.validate_on_submit():
-        print(form)
+        # We have to load the fandoms
+        loaded_fandoms = [
+            db.session.query(Fandom).filter(Fandom.name==fandom).first_or_404()
+            for fandom in form.data['fandom']
+        ]
+        
+        # We have to create the story first
+        story = Story.create(
+            title=form.data['title'],
+            author=current_user,
+            summary=form.data['summary'],
+            fandom=loaded_fandoms
+        )
+
+        # Then we create the first chapter
+        chapter = Chapter.create(
+            content=form.data['content'],
+            story=story
+        )
+        
         return redirect(url_for('home.index'))
     else:
         return render_template('story/post_story.jinja2', form=form)
