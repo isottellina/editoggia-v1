cec938370ad775a1471939e5e4b09496886d4695
Louise <louise@zanier.org>
2020-05-30 18:41:51 +0200

    Split CSS

--- editoggia/models/comment.py
+++ editoggia/models/comment.py
@@ -3,15 +3,23 @@
 # Filename: comments.py
 # Author: Louise <louise>
 # Created: Sat May 30 15:16:18 2020 (+0200)
-# Last-Updated: Sat May 30 15:28:28 2020 (+0200)
+# Last-Updated: Sat May 30 18:40:57 2020 (+0200)
 #           By: Louise <louise>
-# 
+#
+from datetime import datetime
+
 from editoggia.database import db
 from editoggia.models.mixins import CRUDMixin
 
 class Comment(db.Model, CRUDMixin):
     content = db.Column(db.Text, nullable=False)
 
+    created_on = db.Column(db.DateTime(), nullable=False,
+                           default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), nullable=False,
+                           default=datetime.utcnow,
+                           onupdate=datetime.utcnow)
+    
     author_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
     author = db.relationship('User', back_populates='comments')
 
