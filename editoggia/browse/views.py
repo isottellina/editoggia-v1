3bd6da6b81c98d9b85b5746482c611b101ebac7b
Louise <louise@zanier.org>
2020-06-04 17:52:31 +0200

    List stories in a fandom

--- editoggia/browse/views.py
+++ editoggia/browse/views.py
@@ -3,14 +3,14 @@
 # Filename: views.py
 # Author: Louise <louise>
 # Created: Thu Jun  4 16:55:50 2020 (+0200)
-# Last-Updated: Thu Jun  4 17:12:44 2020 (+0200)
+# Last-Updated: Thu Jun  4 17:45:20 2020 (+0200)
 #           By: Louise <louise>
 # 
 from flask import render_template
 
 from editoggia.browse import browse
 from editoggia.database import db
-from editoggia.models import FandomCategory
+from editoggia.models import FandomCategory, Fandom
 
 @browse.route('/fandoms/<category>')
 def fandoms(category):
@@ -22,3 +22,14 @@ def fandoms(category):
                                .first_or_404()
 
     return render_template('browse/fandoms.jinja2', category=fandomcategory)
+
+@browse.route('/fandom/<name>')
+def fandom(name):
+    """
+    Print stories in a fandom.
+    """
+    fandom = db.session.query(Fandom) \
+                       .filter(Fandom.name == name) \
+                       .first_or_404()
+
+    return render_template('browse/fandom.jinja2', fandom=fandom)
