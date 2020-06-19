d4d95c99098bf8721c9282f0e8e3a21483cd26f3
Louise <louise@zanier.org>
2020-06-19 18:44:27 +0200

    Can browse a tag now

--- editoggia/browse/views.py
+++ editoggia/browse/views.py
@@ -3,14 +3,14 @@
 # Filename: views.py
 # Author: Louise <louise>
 # Created: Thu Jun  4 16:55:50 2020 (+0200)
-# Last-Updated: Fri Jun 19 16:33:39 2020 (+0200)
+# Last-Updated: Fri Jun 19 18:43:59 2020 (+0200)
 #           By: Louise <louise>
 # 
 from flask import render_template
 
 from editoggia.browse import browse
 from editoggia.database import db
-from editoggia.models import FandomCategory, Fandom, Story
+from editoggia.models import FandomCategory, Fandom, Story, Tag
 
 @browse.route('/fandoms/<category>')
 def fandoms(category):
@@ -35,4 +35,29 @@ def fandom(name):
                              .filter(Story.fandom.contains(fandom)) \
                              .paginate()
 
-    return render_template('browse/fandom.jinja2', fandom=fandom, stories_page=stories_page)
+    return render_template(
+        'browse/collection.jinja2',
+        endpoint="browse.fandom",
+        collection=fandom,
+        stories_page=stories_page
+    )
+
+@browse.route('/tag/<name>')
+def tag(name):
+    """
+    Print stories in a tag.
+    """
+    tag = db.session.query(Tag) \
+                    .filter(Tag.name == name) \
+                    .first_or_404()
+
+    stories_page = db.session.query(Story) \
+                             .filter(Story.tags.contains(tag)) \
+                             .paginate()
+
+    return render_template(
+        'browse/collection.jinja2',
+        endpoint="browse.tag",
+        collection=tag,
+        stories_page=stories_page
+    )
