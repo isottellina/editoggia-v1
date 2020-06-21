48156ed24cde5a80472a501144db2789b2ee88bb
Louise <louise@zanier.org>
2020-06-21 18:55:34 +0200

    Separated JS

--- editoggia/assets.py
+++ editoggia/assets.py
@@ -3,7 +3,7 @@
 # Filename: assets.py
 # Author: Louise <louise>
 # Created: Sat May  2 05:38:44 2020 (+0200)
-# Last-Updated: Sat Jun 13 21:28:39 2020 (+0200)
+# Last-Updated: Sun Jun 21 15:56:00 2020 (+0200)
 #           By: Louise <louise>
 # 
 """
@@ -23,20 +23,25 @@ css = Bundle(
 )
 
 js = Bundle(
-    'js/ui.js',
     'node_modules/jquery/dist/jquery.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/@fortawesome/fontawesome-free/js/all.js',
-    'node_modules/select2/dist/js/select2.full.min.js',
     output='js/output/js_all.js'
 )
 
+js_story = Bundle(
+    'js/story.js',
+    output='js/output/story.js'
+)
+
 js_select2 = Bundle(
+    'node_modules/select2/dist/js/select2.full.min.js',
     'js/select2.js',
-    output='js/output/js_select2.js'
+    output='js/output/select2.js'
 )
 
 assets = Environment()
 assets.register('css_all', css)
 assets.register('js_all', js)
+assets.register('js_story', js_story)
 assets.register('js_select2', js_select2)
