dc12de55bd49384b235493a21695c8b3d361bf48
Louise <louise@zanier.org>
2020-07-11 00:22:34 +0200

    First functional version of the bleacher

--- editoggia/bleach.py
+++ editoggia/bleach.py
@@ -3,12 +3,12 @@
 # Filename: bleach.py
 # Author: Louise <louise>
 # Created: Tue Jul  7 21:14:43 2020 (+0200)
-# Last-Updated: Fri Jul 10 02:22:46 2020 (+0200)
+# Last-Updated: Sat Jul 11 00:22:23 2020 (+0200)
 #           By: Louise <louise>
 #
 import re
 from bleach.sanitizer import Cleaner
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup, NavigableString, Tag
 
 class Bleacher():
     """
@@ -37,25 +37,6 @@ class Bleacher():
         "em", "hr", "i", "li", "ol", "p", "strong", "ul"
     ]
 
-    # Block-level tags. If something is outside this it will be put
-    # in a <p> tag.
-    BLOCK_LEVEL_TAGS = [
-        "ol", "ul", "blockquote", "code", "p"
-    ]
-
-    # Tags that are independant and can happen outside a block-level.
-    INDEPENDANT_TAGS = [
-        "hr"
-    ]
-
-    # Tags that should just be ignored and only their content treated.
-    IGNORE_TAGS = [
-        "[document]", "html", "body"
-    ]
-
-    NEWLINES_RE = re.compile(r'\n+\s*\n+')
-    SPACES_RE = re.compile(r'[ ]+')
-
     def __init__(self):
         """
         Creates the object.
@@ -63,52 +44,6 @@ class Bleacher():
         self.cleaner = Cleaner(
             tags=self.ALLOWED_TAGS
         )
-
-    def open_tag(self, element, out_html=""):
-        """
-        Adds an open tag to the out_html string,
-        with tag and attributes of the element given.
-        Returns the string
-        """
-        return out_html + "<{tag}{attrs}>".format(
-            tag=element.name,
-            attrs="".join([
-                f' {attr}="{element.attrs[attr]}"' for attr in element.attrs
-            ])
-        )
-
-    def close_tag(self, element, out_html=""):
-        """
-        Adds a closing tag to the out_html string.
-        Returns the string.
-        """
-        return out_html + f'</{element.name}>'
-        
-    def traverse_node(self, element, out_html=""):
-        """
-        Traverse every node in the HTML, and
-        produces a new HTML string. We work on
-        a string since the resulting HTML might
-        have a completely different structure.
-        """
-        # Treatment if it's a string.
-        if type(element) == NavigableString:
-            return out_html + element.string
-        
-        # This is the treatment if this is a normal tag.
-        # We recreate the opening tag if we're not supposed to
-        # ignore it
-        if element.name not in self.IGNORE_TAGS:
-            out_html = self.open_tag(element, out_html)
-
-        for child in element.children:
-            out_html = self.traverse_node(child, out_html)
-            
-        # Now we close the tag
-        if element.name not in self.IGNORE_TAGS:
-            out_html = self.close_tag(element, out_html)
-
-        return out_html
 
     def clean(self, text):
         """
@@ -119,7 +54,167 @@ class Bleacher():
         soup = BeautifulSoup(cleaned_text, features="lxml")
 
         # Treat HTML to create a new document
-        new_doc = self.traverse_node(soup)
-        print(new_doc)
+        new_doc = HTMLProducer()
+        new_doc.traverse_node(soup)
+        print("+++++++++++++++++++++++++++++++++++++++++")
+        print(str(new_doc))
 
         return cleaned_text
+
+class HTMLProducer():
+    """
+    The object that produces HTML, used by the Bleacher.
+    It uses internal state, so it's a separate object.
+    """
+    # Block-level tags. If something is outside this it will be put
+    # in a <p> tag.
+    BLOCK_LEVEL_TAGS = [
+        "ol", "ul", "blockquote", "code", "p", "hr"
+    ]
+
+    # Tags that are independant and can happen outside a block-level.
+    INDEPENDANT_TAGS = [
+        "hr"
+    ]
+
+    # Tags that should just be ignored and only their content treated.
+    # They're top level tags.
+    IGNORE_TAGS = [
+        "[document]", "html", "body"
+    ]
+
+    # Tags that should be conserved as-is
+    CONSERVE_TAGS = ["a", "abbr", "acronym", "address", "br",
+                     "dl", "hr", "ol", "p", "pre", "blockquote", "ul"]
+
+    SINGLE_NEWLINE_RE = re.compile(r'\n')
+    DOUBLE_NEWLINES_RE = re.compile(r'\n+\s*\n+')
+    
+    def __init__(self):
+        # The stack of tags
+        self.stack = []
+
+        # String to produce.
+        self.out_html = ""
+
+    def open_tag(self, element):
+        """
+        Adds an open tag to the out_html string,
+        with tag and attributes of the element given.
+        Returns the string
+        """
+        self.out_html += "<{tag}{attrs}>".format(
+            tag=element.name,
+            attrs="".join([
+                f' {attr}="{element.attrs[attr]}"' for attr in element.attrs
+            ])
+        )
+
+    def close_tag(self, tag_name):
+        """
+        Adds a closing tag to the out_html string.
+        Returns the string.
+        """
+        self.out_html += f'</{tag_name}>'
+
+    def open_p(self):
+        """
+        Create a paragraph.
+        """
+        self.stack.append('p')
+        self.out_html += "<p>"
+
+    def close_block(self):
+        """
+        If we are creating a paragraph, close it.
+        """
+        for item in reversed(self.stack):
+            self.stack.pop()
+            self.close_tag(item)
+
+            if item in self.BLOCK_LEVEL_TAGS:
+                return
+
+    def close_until(self, tag_name):
+        """
+        Close until we encounter this element name
+        in the stack.
+        """
+        for item in reversed(self.stack):
+            self.stack.pop()
+            self.close_tag(item)
+
+            if item == tag_name:
+                return
+
+    def in_block(self):
+        """
+        Are we currently in a block?
+        """
+        for tag in self.stack:
+            if tag in self.BLOCK_LEVEL_TAGS:
+                return True
+        return False
+
+    def add_string(self, string):
+        """
+        Adds a string to the HTML.
+        """
+        self.out_html += string
+
+    def traverse_node(self, element):
+        """
+        Traverse every node in the HTML, and
+        produces a new HTML string. We work on
+        a string since the resulting HTML might
+        have a completely different structure.
+        """
+        # Some tags we just keep as-is.
+        if type(element) == Tag and element.name in self.CONSERVE_TAGS:
+            if element.name in self.BLOCK_LEVEL_TAGS and self.in_block():
+                self.close_block()
+                self.add_string(str(element))
+            elif element.name not in self.BLOCK_LEVEL_TAGS and not self.in_block():
+                self.add_string("<p>" + str(element) + "</p>")
+            else:
+                print(str(element))
+                self.add_string(str(element))
+
+            return
+
+        # If it's a string just add it (and open a paragraph if we need to)
+        if type(element) == NavigableString:
+            # We only open a paragraph for strings not empty
+            if not self.in_block() and element.string.strip() != "":
+                self.open_p()
+            self.add_string(element.string)
+
+            return
+        
+        # Some tags we descend into. This now must be a tag.
+        if element.name not in self.IGNORE_TAGS:
+            print('<' + element.name + '>')
+            # If it's an inline tag and we're not in a block, create a tag.
+            if element.name not in self.BLOCK_LEVEL_TAGS and not self.in_block():
+                self.open_p()
+            self.open_tag(element)
+            self.stack.append(element.name)
+
+        for child in element.children:
+            self.traverse_node(child)
+
+        self.close_until(element.name)
+
+    def process_html(self, html):
+        """
+        Wraps the traverse_node function to ensure that
+        all tags are closed at the end.
+        """
+        self.traverse_node(html)
+
+        for element in reversed(self.stack):
+            self.close_tag(element)
+        self.stack = []
+
+    def __str__(self):
+        return self.out_html
