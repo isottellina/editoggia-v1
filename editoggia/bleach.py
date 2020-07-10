b01bda55dcedb6cb18429072ae2c554e281e6a10
Louise <louise@zanier.org>
2020-07-10 02:23:33 +0200

    Basic work on the bleacher. Nothing conclusive but structure.

--- editoggia/bleach.py
+++ editoggia/bleach.py
@@ -3,12 +3,12 @@
 # Filename: bleach.py
 # Author: Louise <louise>
 # Created: Tue Jul  7 21:14:43 2020 (+0200)
-# Last-Updated: Thu Jul  9 20:37:29 2020 (+0200)
+# Last-Updated: Fri Jul 10 02:22:46 2020 (+0200)
 #           By: Louise <louise>
 #
 import re
 from bleach.sanitizer import Cleaner
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 class Bleacher():
     """
@@ -31,17 +31,26 @@ class Bleacher():
         (some block-level tag enclosing text)
       -
     """
+    # Only tags that should be conserved.
     ALLOWED_TAGS = [
         "a", "abbr", "b", "br", "blockquote", "code",
         "em", "hr", "i", "li", "ol", "p", "strong", "ul"
     ]
 
+    # Block-level tags. If something is outside this it will be put
+    # in a <p> tag.
     BLOCK_LEVEL_TAGS = [
         "ol", "ul", "blockquote", "code", "p"
     ]
 
-    INDEPENDANT_TAG = [
+    # Tags that are independant and can happen outside a block-level.
+    INDEPENDANT_TAGS = [
         "hr"
+    ]
+
+    # Tags that should just be ignored and only their content treated.
+    IGNORE_TAGS = [
+        "[document]", "html", "body"
     ]
 
     NEWLINES_RE = re.compile(r'\n+\s*\n+')
@@ -55,11 +64,51 @@ class Bleacher():
             tags=self.ALLOWED_TAGS
         )
 
-    def traverse_node(out_html, element):
+    def open_tag(self, element, out_html=""):
+        """
+        Adds an open tag to the out_html string,
+        with tag and attributes of the element given.
+        Returns the string
+        """
+        return out_html + "<{tag}{attrs}>".format(
+            tag=element.name,
+            attrs="".join([
+                f' {attr}="{element.attrs[attr]}"' for attr in element.attrs
+            ])
+        )
+
+    def close_tag(self, element, out_html=""):
+        """
+        Adds a closing tag to the out_html string.
+        Returns the string.
+        """
+        return out_html + f'</{element.name}>'
+        
+    def traverse_node(self, element, out_html=""):
         """
         Traverse every node in the HTML, and
-        produces a new HTML string.
+        produces a new HTML string. We work on
+        a string since the resulting HTML might
+        have a completely different structure.
         """
+        # Treatment if it's a string.
+        if type(element) == NavigableString:
+            return out_html + element.string
+        
+        # This is the treatment if this is a normal tag.
+        # We recreate the opening tag if we're not supposed to
+        # ignore it
+        if element.name not in self.IGNORE_TAGS:
+            out_html = self.open_tag(element, out_html)
+
+        for child in element.children:
+            out_html = self.traverse_node(child, out_html)
+            
+        # Now we close the tag
+        if element.name not in self.IGNORE_TAGS:
+            out_html = self.close_tag(element, out_html)
+
+        return out_html
 
     def clean(self, text):
         """
@@ -68,11 +117,9 @@ class Bleacher():
         """
         cleaned_text = self.cleaner.clean(text)
         soup = BeautifulSoup(cleaned_text, features="lxml")
-        # If the parser created a body tag, we use that instead.
-        text_body = soup.find('body') if soup.find('body') else soup
 
-        # Process each element, which are either
-        for element in text_body.children:
-            print(element)
+        # Treat HTML to create a new document
+        new_doc = self.traverse_node(soup)
+        print(new_doc)
 
-        return str(soup)
+        return cleaned_text
