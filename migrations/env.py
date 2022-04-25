02a3654d1dc173be7f084058987417b7193d5e3e
Louise <louise@zanier.org>
2022-04-25 22:58:52 +0200

    [migrations] Listen to a connection attribute

--- migrations/env.py
+++ migrations/env.py
@@ -72,23 +72,34 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info("No changes in schema detected.")
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connection = config.attributes.get("connection", None)
 
-    with connectable.connect() as connection:
+    if connection is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
+
+        with connectable.connect() as connection:
+            context.configure(
+                connection=connection,
+                target_metadata=target_metadata,
+                process_revision_directives=process_revision_directives,
+                **current_app.extensions["migrate"].configure_args
+            )
+
+            with context.begin_transaction():
+                context.run_migrations()
+    else:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
             process_revision_directives=process_revision_directives,
             **current_app.extensions["migrate"].configure_args
         )
-
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
