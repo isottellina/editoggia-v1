ad1d7f726ac78978bb27570616933e3c08ff89b8
Louise <louise@zanier.org>
2022-04-30 13:03:06 +0200

    [tests] Fixed browse tests

--- tests/test_browse.py
+++ tests/test_browse.py
@@ -13,158 +13,145 @@ from editoggia.models import Story, Fandom, Tag
 
 
 class TestBrowse(EditoggiaTestCase):
-    def test_fandoms(self):
+    def test_fandoms(self, client):
         """
         Tests the fandoms view, printing all fandoms in a category.
         """
-        rv = self.client.get("/browse/fandoms/Other")
+        rv = client.get("/browse/fandoms/Other")
 
-        self.assert200(rv)
-        self.assertIn(b"Original Work", rv.data)
+        assert rv.status_code == 200
+        assert b"Original Work" in rv.data
 
-    def test_collection_title(self):
+    def test_collection_title(self, client, create_story):
         """
         Tries to display stories in order of title.
         """
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
         least_title = min(stories[0].title, stories[1].title)
         most_title = max(stories[0].title, stories[1].title)
 
-        rv = self.client.get("/browse/fandom/Original Work?order_by=title")
-        self.assert200(rv)
-        self.assertTrue(
-            rv.data.find(least_title.encode()) < rv.data.find(most_title.encode())
-        )
+        rv = client.get("/browse/fandom/Original Work?order_by=title")
+        assert rv.status_code == 200
+        assert rv.data.find(least_title.encode()) < rv.data.find(most_title.encode())
 
-    def test_collection_author(self):
+    def test_collection_author(self, client, create_user, create_story):
         """
         Tries to display stories in order of title.
         """
-        new_user, _ = self.create_user(self.user.username + "2")
+        user0, _ = create_user()
+        user1, _ = create_user(user0.username + "2")
 
-        least_author = self.create_story()
-        most_author = self.create_story(author=new_user)
+        least_author = create_story(author=user0)
+        most_author = create_story(author=user1)
 
-        rv = self.client.get("/browse/fandom/Original Work?order_by=author")
-        self.assert200(rv)
-        self.assertTrue(
-            rv.data.find(least_author.title.encode())
-            < rv.data.find(most_author.title.encode())
-        )
+        rv = client.get("/browse/fandom/Original Work?order_by=author")
+        assert rv.status_code == 200
+        assert rv.data.find(least_author.title.encode()) < rv.data.find(most_author.title.encode())
 
-    def test_collection_date_updated(self):
+    def test_collection_date_updated(self, client, create_story):
         """
         Tries to display stories in order of dates.
         """
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
-        rv = self.client.get("/browse/fandom/Original Work")
-        self.assert200(rv)
-        self.assertTrue(
-            rv.data.find(stories[1].title.encode())
-            < rv.data.find(stories[0].title.encode())
-        )
+        rv = client.get("/browse/fandom/Original Work")
+        assert rv.status_code == 200
+        assert rv.data.find(stories[1].title.encode()) < rv.data.find(stories[0].title.encode())
 
-    def test_collection_hits(self):
+    def test_collection_hits(self, client, create_user, create_story):
         """
         Tries to display stories in order of hits.
         """
-        stories = self.create_stories(2)
-        self.hit(stories[0])
+        user, _ = create_user()
+        stories = [create_story() for _ in range(2)]
+        self.hit(user, stories[0])
 
-        rv = self.client.get("/browse/fandom/Original Work?order_by=hits")
-        self.assert200(rv)
-        self.assertTrue(
-            rv.data.find(stories[0].title.encode())
-            < rv.data.find(stories[1].title.encode())
-        )
+        rv = client.get("/browse/fandom/Original Work?order_by=hits")
+        assert rv.status_code == 200
+        assert rv.data.find(stories[0].title.encode()) < rv.data.find(stories[1].title.encode())
 
-    def test_collection_likes(self):
+    def test_collection_likes(self, client, create_user, create_story):
         """
         Tries to display stories in order of likes.
         """
-        stories = self.create_stories(2)
-        self.like(stories[0])
+        user, _ = create_user()
+        stories = [create_story() for _ in range(2)]
+        self.like(user, stories[0])
 
-        rv = self.client.get("/browse/fandom/Original Work?order_by=likes")
-        self.assert200(rv)
-        self.assertTrue(
-            rv.data.find(stories[0].title.encode())
-            < rv.data.find(stories[1].title.encode())
-        )
+        rv = client.get("/browse/fandom/Original Work?order_by=likes")
+        assert rv.status_code == 200
+        assert rv.data.find(stories[0].summary.encode()) < rv.data.find(stories[1].summary.encode())
 
-    def test_collection_bad_order(self):
+    def test_collection_bad_order(self, client):
         """
         Bad ordering.
         """
-        stories = self.create_stories(2)
+        rv = client.get("/browse/fandom/Original Work?order_by=bad")
+        assert rv.status_code == 400
 
-        rv = self.client.get("/browse/fandom/Original Work?order_by=bad")
-        self.assert400(rv)
-
-    def test_collection_rating(self):
+    def test_collection_rating(self, client, create_story):
         """
         Tests we only get stories rated as we asked.
         """
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
         # Set rating
         stories[1].update(rating="Explicit")
 
-        rv = self.client.get("/browse/fandom/Original Work?rating=Explicit")
+        rv = client.get("/browse/fandom/Original Work?rating=Explicit")
 
-        self.assert200(rv)
-        self.assertNotIn(stories[0].title.encode(), rv.data)
-        self.assertIn(stories[1].title.encode(), rv.data)
+        assert rv.status_code == 200
+        assert stories[0].title.encode() not in rv.data
+        assert stories[1].title.encode() in rv.data
 
-    def test_collection_included_fandoms(self):
+    def test_collection_included_fandoms(self, client, create_story):
         """
         Tests we can include another fandom in the search and only get
         twice-tagged stories.
         """
         other_fandom = Fandom.create(name="Fandom2")
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
         # Add fandom to story
-        stories[1].update(fandom=[self.fandom, other_fandom])
+        stories[1].fandom.append(other_fandom)
 
-        rv = self.client.get("/browse/fandom/Original Work?included_fandom=Fandom2")
+        rv = client.get("/browse/fandom/Original Work?included_fandom=Fandom2")
 
-        self.assert200(rv)
-        self.assertNotIn(stories[0].title.encode(), rv.data)
-        self.assertIn(stories[1].title.encode(), rv.data)
+        assert rv.status_code == 200
+        assert stories[0].title.encode() not in rv.data
+        assert stories[1].title.encode() in rv.data
 
-    def test_collection_included_tags(self):
+    def test_collection_included_tags(self, client, create_story):
         """
         Tests we can include a tag in the search and only get stories
         tagged with it.
         """
         other_tag = Tag.create(name="Tag1")
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
         # Add tag to story
         stories[1].update(tags=[other_tag])
 
-        rv = self.client.get("/browse/fandom/Original Work?included_tags=Tag1")
+        rv = client.get("/browse/fandom/Original Work?included_tags=Tag1")
 
-        self.assert200(rv)
-        self.assertNotIn(stories[0].title.encode(), rv.data)
-        self.assertIn(stories[1].title.encode(), rv.data)
+        assert rv.status_code == 200
+        assert stories[0].title.encode() not in rv.data
+        assert stories[1].title.encode() in rv.data
 
-    def test_collection_excluded_tags(self):
+    def test_collection_excluded_tags(self, client, create_story):
         """
         Tests we can exclude a tag in the search and only get stories
         that don't have it
         """
         other_tag = Tag.create(name="Tag1")
-        stories = self.create_stories(2)
+        stories = [create_story() for _ in range(2)]
 
         # Add tag to story
         stories[1].update(tags=[other_tag])
 
-        rv = self.client.get("/browse/fandom/Original Work?excluded_tags=Tag1")
+        rv = client.get("/browse/fandom/Original Work?excluded_tags=Tag1")
 
-        self.assert200(rv)
-        self.assertIn(stories[0].title.encode(), rv.data)
-        self.assertNotIn(stories[1].title.encode(), rv.data)
+        assert rv.status_code == 200
+        assert stories[0].title.encode() in rv.data
+        assert stories[1].title.encode() not in rv.data
